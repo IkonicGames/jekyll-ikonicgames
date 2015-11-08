acc96694396ab8d8ba04990385e16a917f781ce6
Erin Gunn <ikonone@gmail.com>
2015-11-07 23:21:15 -0500

    Cleaned css.  First Pass???

--- app/_site/contact/contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $website = $_POST['website'];
-    $message = $_POST['message'];
-
-    $to = "contact@ikonicgames.com";
-    $subject = substr($message, 20);
-?>
